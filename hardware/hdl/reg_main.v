8432aea87a43ea8f4f89e519bcb8db6c70565b7b
jpcrypt <jpthibault@newae.com>
2022-03-09 16:01:41 -0500

    Support for multiple trace-generated triggers.

--- hardware/hdl/reg_main.v
+++ hardware/hdl/reg_main.v
@@ -72,7 +72,6 @@ module reg_main #(
    output wire         O_arm,
    output wire         O_reg_arm,
    output wire         O_arm_pulse,
-   output wire         O_no_arm,
    output wire         O_capture_off,
    input  wire         I_flushing,
    output wire [pCAPTURE_LEN_WIDTH-1:0] O_capture_len,
@@ -95,6 +94,7 @@ module reg_main #(
    output wire [pALL_TRIGGER_DELAY_WIDTHS-1:0]  O_trigger_delay,
    output wire [pALL_TRIGGER_WIDTH_WIDTHS-1:0]  O_trigger_width,
    output wire [pNUM_TRIGGER_WIDTH-1:0]         O_num_triggers,
+   input  wire [pNUM_TRIGGER_WIDTH-1:0]         I_triggers_generated,
    output wire                                  O_trigger_enable,
 
 
@@ -122,7 +122,6 @@ module reg_main #(
    reg  [17:0] fifo_data_r;
    reg  reg_arm;
    reg  reg_arm_r;
-   reg  reg_no_arm;
    reg  reg_capture_off;
    reg  capture_now;
    reg  capture_now_r;
@@ -175,7 +174,6 @@ module reg_main #(
    assign O_arm = reg_arm_r & ~I_flushing;
    assign O_arm_pulse = reg_arm & ~reg_arm_r;
    assign O_reg_arm = reg_arm;
-   assign O_no_arm = reg_no_arm;
    assign O_capture_off = reg_capture_off;
    assign O_userio_pwdriven = reg_userio_pwdriven;
    assign O_userio_drive_data = reg_userio_drive_data;
@@ -193,12 +191,12 @@ module reg_main #(
             `REG_SNIFF_FIFO_STAT: reg_read_data = {2'b00, I_fifo_status};
             `REG_FE_SELECT: reg_read_data = fe_select;
             `REG_ARM: reg_read_data = reg_arm;
-            `REG_NO_ARM: reg_read_data = reg_no_arm;
             `REG_CAPTURE_OFF: reg_read_data = reg_capture_off;
             `REG_TRIGGER_ENABLE: reg_read_data = reg_trigger_enable;
             `REG_TRIGGER_DELAY: reg_read_data = reg_trigger_delay[reg_bytecnt*8 +: 8]; // warning: repeated access may not work as expected
             `REG_TRIGGER_WIDTH: reg_read_data = reg_trigger_width[reg_bytecnt*8 +: 8]; // warning: repeated access may not work as expected
-            `REG_NUM_TRIGGERS: reg_read_data = {4'b0, reg_num_triggers};
+            `REG_NUM_TRIGGERS: reg_read_data = reg_num_triggers[reg_bytecnt*8 +: 8];
+            `REG_TRIGGERS_GENERATED: reg_read_data = I_triggers_generated[reg_bytecnt*8 +: 8];
             `REG_TRIG_CLK_PHASE_SHIFT: reg_read_data = {7'b0, phaseshift_active};
             `REG_CAPTURE_LEN: reg_read_data = reg_capture_len[reg_bytecnt*8 +: 8]; // warning: repeated access may not work as expected
             `REG_COUNT_WRITES: reg_read_data = reg_count_writes;
@@ -328,7 +326,6 @@ module reg_main #(
       if (fpga_reset) begin
          fe_select <= `FE_USB;
          reg_arm <= 1'b0;
-         reg_no_arm <= 1'b0;
          reg_capture_off <= 1'b0;
          reg_arm_r <= 1'b0;
          reg_trigger_enable <= 0;
@@ -367,7 +364,7 @@ module reg_main #(
                `REG_TRIGGER_ENABLE: reg_trigger_enable <= write_data;
                `REG_TRIGGER_DELAY: reg_trigger_delay[reg_bytecnt*8 +: 8] <= write_data; // warning: repeated access may not work as expected
                `REG_TRIGGER_WIDTH: reg_trigger_width[reg_bytecnt*8 +: 8] <= write_data; // warning: repeated access may not work as expected
-               `REG_NUM_TRIGGERS: reg_num_triggers <= write_data[pNUM_TRIGGER_WIDTH-1:0];
+               `REG_NUM_TRIGGERS: reg_num_triggers[reg_bytecnt*8 +: 8] <= write_data;
                `REG_CAPTURE_LEN: reg_capture_len[reg_bytecnt*8 +: 8] <= write_data; // warning: repeated access may not work as expected
                `REG_COUNT_WRITES: reg_count_writes <= write_data;
                `REG_COUNTER_QUICK_START: reg_counter_quick_start <= write_data;
@@ -379,7 +376,6 @@ module reg_main #(
                `REG_CAPTURE_WHILE_TRIG: reg_capture_while_trig <= write_data[0];
                `REG_MAX_TIMESTAMP: reg_max_timestamp[reg_bytecnt[0]*8 +: 8] <= write_data;
                `REG_LED_SELECT: reg_led_select <= write_data[0];
-               `REG_NO_ARM: reg_no_arm <= write_data[0];
                `REG_CAPTURE_OFF: reg_capture_off <= write_data[0];
             endcase
          end
